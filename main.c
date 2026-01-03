cb0d5fc15d183240e721928972fdc938484b2b2e
nicolovinci <12999303@itisrossi.vi.it>
2026-01-03 15:26:04 +0100

    Done pipe and fork

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int fileDescriptor[2];
+    if (pipe(fileDescriptor) == -1) {
+        perror("pipe");
+        return 1;
+    }
+    pid_t child;
+    if ((child = fork()) == -1) {
+        perror("fork");
+        return 1;
+    }
+    if (child == 0) {
+
+    } else {
+
+    }
     return 0;
 }
